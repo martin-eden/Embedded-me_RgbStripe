bf0adbd721d5a1ba20510e77aebc211d4f68a045
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-11-12 18:48:00 +0200

    [~] [Examples.DisplayAnalogRead]

--- examples/DisplayAnalogRead/DisplayAnalogRead.ino
+++ examples/DisplayAnalogRead/DisplayAnalogRead.ino
@@ -2,73 +2,23 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-12-20
+  Last mod.: 2025-11-12
 */
 
 #include <me_RgbStripe.h>
 
 #include <me_BaseTypes.h>
-#include <me_Uart.h>
 #include <me_Console.h>
+#include <me_Delays.h>
 
 const TUint_1 Stripe_OutputPin = 2;
 const TUint_1 Stripe_Length = 60;
+const TUint_2 DataMemSize = Stripe_Length * sizeof(me_RgbStripe::TColor);
+
+TUint_1 DataMem[DataMemSize];
+const TAddressSegment DataSeg = { (TAddress) &DataMem, sizeof(DataMem) };
 
 me_RgbStripe::TRgbStripe Stripe;
-
-void setup()
-{
-  me_Uart::Init(me_Uart::Speed_115k_Bps);
-  Stripe.Init(Stripe_OutputPin, Stripe_Length);
-}
-
-void loop()
-{
-  const TUint_1 ReadingPin = A0;
-  const TUint_1 ReadingInterval_ms = 40;
-
-  TUint_2 ReadingValue = analogRead(ReadingPin);
-
-  DisplayReadings(ReadingValue);
-
-  delay(ReadingInterval_ms);
-}
-
-/*
-  Process analog reading value
-*/
-void DisplayReadings(
-  TUint_2 AnalogValue
-)
-{
-  const TUint_2 MaxGapToIgnore = 20;
-
-  static TUint_2 LastPrintedValue = 0;
-
-  TUint_2 Gap;
-
-  Gap = GetGap(AnalogValue, LastPrintedValue);
-
-  if (Gap > MaxGapToIgnore)
-  {
-    // Console.Print(Gap);
-
-    DisplayValue(AnalogValue);
-
-    LastPrintedValue = AnalogValue;
-  }
-}
-
-TUint_2 GetGap(
-  TUint_2 A,
-  TUint_2 B
-)
-{
-  if (A < B)
-    return B - A;
-
-  return A - B;
-}
 
 /*
   Display value as pixel in LED stripe
@@ -117,6 +67,62 @@ void DisplayValue(
   Stripe.Display();
 
   LastPixelIndex = CurPixelIndex;
+}
+
+/*
+  Process analog reading value
+*/
+void DisplayReadings(
+  TUint_2 AnalogValue
+)
+{
+  const TUint_2 MaxGapToIgnore = 20;
+
+  static TUint_2 LastPrintedValue = 0;
+
+  TUint_2 Gap;
+
+  Gap = GetGap(AnalogValue, LastPrintedValue);
+
+  if (Gap > MaxGapToIgnore)
+  {
+    // Console.Print(Gap);
+
+    DisplayValue(AnalogValue);
+
+    LastPrintedValue = AnalogValue;
+  }
+}
+
+TUint_2 GetGap(
+  TUint_2 A,
+  TUint_2 B
+)
+{
+  if (A < B)
+    return B - A;
+
+  return A - B;
+}
+
+void setup()
+{
+  me_Delays::Init();
+  Console.Init();
+  Stripe.Init(Stripe_OutputPin, DataSeg);
+  Stripe.Clear();
+}
+
+void loop()
+{
+  const TUint_1 ReadingPin = A0;
+  const TUint_1 ReadingInterval_ms = 40;
+
+  TUint_2 ReadingValue = analogRead(ReadingPin);
+
+  DisplayReadings(ReadingValue);
+
+  me_Delays::Delay_Ms(ReadingInterval_ms);
 }
 
 /*
